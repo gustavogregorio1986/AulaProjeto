216f44d137224c5f876bed26a75a617d33f3e293
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-03-04 12:45:37 -0300

    Desenvolvimento de input entrada por teclado

---
+++ Aula03/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Aula03
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int number = 30;
+
+            number = 20;
+
+            Console.WriteLine("O valor antigo da variável é: " + number);
+
+            number = 10;
+
+            Console.WriteLine(number);
+
+            Console.WriteLine("O valor novo da variável é: " + number);
+
+            string name = "Antonio";
+
+            name = "Antonio Rocha";
+
+            Console.WriteLine(name);
+
+            const int number1 = 1000;
+
+            Console.WriteLine(number1);
+
+            Console.WriteLine("O valor da constantel é: " + number1);
+
+            string variable = "None";
+
+            Console.WriteLine("O valor da variável é: " + variable);
+
+            dynamic variable1 = 30;
+
+            variable1 = "Antonio";
+
+            Console.WriteLine(variable1);
+        }
+    }
+}
---
+++ Aula04/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Aula04
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("----------CADASTRO DE USUÁRIO-----------");
+
+            //Solicitar o nome do Usuario
+            Console.WriteLine("Digite o nome: ");
+            string name = Console.ReadLine();
+
+            //Solicitar a Identidade do usario
+            Console.WriteLine("Digite Sua idade: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("-----------------------------------------");
+
+            Console.WriteLine("Nome do Usuário: " + name);
+            Console.WriteLine("Age do Usuário: " + age);
+        }
+    }
+}
