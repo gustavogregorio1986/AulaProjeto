3dfd9788117d2fc7be12d28dcbdaf1c05576d9c8
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-03-04 15:12:16 -0300

    Desenvolvendo projeto simples de impar ou par

---
+++ Aula05/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Aula05
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("--------Calculadora Bem simples-------------------");
+
+            Console.WriteLine("Digite o numero 1: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o numero 2: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+            int sum = number1 + number2;
+            int subtracao = number1 - number2;
+            int multiplicacao = number1 * number2;
+            int division = number2 / number1;
+            int modulo = number2 % number1;
+
+            Console.WriteLine("O valor da soma é: " + sum);
+            Console.WriteLine("O valor da subtração é: " + subtracao);
+            Console.WriteLine("O valor da multiplicação é: " + multiplicacao);
+            Console.WriteLine("O valor da divisão é: " + division);
+            Console.WriteLine("O resto da divisao é: " + modulo);
+
+            Console.WriteLine(10 + 30);
+        }
+    }
+}
---
+++ Aula06/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Aula06
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("==============Verificador de Numeros pares ou Impares=========");
+            Console.WriteLine("Digite o numero: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            int result = numero % 2;
+
+            if(result == 0)  
+            {
+               Console.WriteLine("O numero é par");
+            }
+            else
+            {
+                Console.WriteLine("O numero é impar");
+            }
+    }
+}
