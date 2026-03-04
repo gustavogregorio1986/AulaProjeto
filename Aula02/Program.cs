30f284cc222f0838a20be0401f38780fd702e8bd
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-03-04 10:18:08 -0300

    Add project files.

---
+++ Aula02/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Aula02
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string myName = "Catarina";
+
+            Console.WriteLine(myName);
+
+            int number = 20;
+
+            Console.WriteLine(number);
+
+            double numberDouble = 10.50;
+            Console.WriteLine(numberDouble);
+
+            char character = 'a';
+
+            Console.WriteLine(character);
+
+            bool boolean = true;
+
+            Console.WriteLine(boolean);
+
+
+        }
+    }
+}
