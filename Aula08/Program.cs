510662bee8543bea0b653a0d2f6e2467d2a82d46
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-03-05 15:39:01 -0300

    Desenvolver os operadores logicos na logica de programação

---
+++ Aula08/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace Aula08
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("==============Operações Relacionais=============");
+            Console.WriteLine("Digite o primeiro numero(x): ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo numero(y): ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            string result;
+
+            //Operadors de Igualdade (==)
+            if(x == y)
+            {
+                result = "x é igual a y.";
+            }
+            else
+            {
+                result = "x diferente a y.";
+            }
+
+            Console.WriteLine("\nIgualdade (==): " + result);
+
+            //Operador de Diferença (!=)
+            if(x != y)
+            {
+                result = "x diferente a y.";
+            }
+            else
+            {
+                result = "x é igual a y.";
+            }
+
+            Console.WriteLine("\nDiferença (!=): " + result);
+
+            //Opreador amiro que (>)
+            if(x > y)
+            {
+                result = "x é maior que y.";
+            }
+            else
+            {
+                result = "x não é maior que y.";
+            }
+
+            Console.WriteLine("\nMaior que (>): " + result);
+
+            //Opreador menor que
+            if(x < y)
+            {
+                result = "x é menor que y.";
+            }
+            else
+            {
+                result = "x não é menor que y.";
+            }
+
+            Console.WriteLine("\nMenor que (<): " + result);
+
+            //Opreador maior ou igual a (>=)
+            if (x >= y)
+            {
+                result = "x é maior ou igual que y.";
+            }
+            else
+            {
+                result = "x não é maior que y.";
+            }
+
+            Console.WriteLine("\nMaior ou igual que (>=): " + result);
+
+            //Opreador menor ou igual a (>=)
+            if (x <= y)
+            {
+                result = "x é menor que y.";
+            }
+            else
+            {
+                result = "x não é menor ou igual que y.";
+            }
+
+            Console.WriteLine("\nMenor ou igual que (<=): " + result);
+        }
+    }
+}
---
+++ Aula09/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Aula09
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("=====Opreadores logicos===========");
+
+            bool isLogged = true;
+            bool isAdminAcess = false;
+
+            Console.WriteLine("\nInforme do usuário: ");
+            Console.WriteLine("Iusuario logado: " + isLogged);
+            Console.WriteLine("Acesso de admin: " + isAdminAcess);
+
+            //Opreador (&&) - E
+            if (isLogged && isAdminAcess)
+            {
+                Console.WriteLine("Acesso ao painel de administrador concebido");
+            }
+            else
+            {
+                Console.WriteLine("Acesso ao painel de administrador negado");
+            }
+
+            //Operador (||) - OU
+            if (isLogged || isAdminAcess)
+            {
+                Console.WriteLine("Acesso ao painel de administrador concebido");
+            }
+            else
+            {
+                Console.WriteLine("Acesso ao painel de administrador negado");
+            }
+
+            //Opreador (!) - "Não..."
+            if (!isLogged)
+            {
+                Console.WriteLine("Usuário não esta logado");
+            }
+            else
+            {
+                Console.WriteLine("Usuário esta logado");
+            }
+        }
+    }
+}
