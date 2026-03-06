0acf96c5a161f66b0cbd78f2864f2772df61516a
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-03-06 11:33:01 -0300

    Desenvolvimento do switch case na logica de programação

---
+++ Aula10/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Aula10
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            bool continuar;
+
+            do
+            {
+                Console.WriteLine("============Calculadora Simples===============");
+                Console.WriteLine("Digite o primeiro numero: ");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Digite o segundo numero: ");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+
+                //Selecionar o tipo de operação
+                Console.WriteLine("\nEscolha uma opção: ");
+                Console.WriteLine("1 - Adição (+)");
+                Console.WriteLine("2 - Subtração (-)");
+                Console.WriteLine("3 - Multiplicação (*)");
+                Console.WriteLine("4 - Divisão (/)");
+                Console.WriteLine("Digite o numero da operação");
+                int operaqtion = Convert.ToInt32(Console.ReadLine());
+
+                //Condições
+                if (operaqtion == 1)
+                {
+                    Console.WriteLine($"O resultado da adição é: {num1 + num2}");
+                }
+                else if (operaqtion == 2)
+                {
+                    Console.WriteLine($"O resultado da subtração é: {num1 - num2}");
+                }
+                else if (operaqtion == 3)
+                {
+                    Console.WriteLine($"O resultado da multiplicação é: {num1 * num2}");
+                }
+                else if (operaqtion == 4)
+                {
+                    if (num2 != 0)
+                    {
+                        Console.WriteLine($"O resultado da divisão é: {num1 / num2}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Erro: Divisão por zero não é permitida.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Opção inválida. Por favor, escolha uma operação válida.");
+                }
+
+                // Perguntar se deseja repetir
+                Console.WriteLine("\nDeseja realizar outra operação? (s/n)");
+                string resposta = Console.ReadLine().ToLower();
+                continuar = (resposta == "s");
+
+            } while (continuar);
+
+            Console.WriteLine("Programa encerrado. Obrigado por usar a calculadora!");
+        }
+    }
+}
---
+++ Aula11/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace Aula11
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //Ex1
+            //int number = 1;
+
+            //switch (number)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Number is 1");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Number is 2");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Number is not 1 or 2");
+            //        break;
+            //}
+
+            //Ex2
+            //bool boolValue = true;
+
+            //switch (boolValue)
+            //{
+            //    case true:
+            //        Console.WriteLine("O valor é true");
+            //        break;
+            //    case false:
+            //        Console.WriteLine("O valor é false");
+            //        break;
+
+            //}
+
+            //Ex3
+            //Console.WriteLine("==========Dia da semana==============");
+            //Console.WriteLine("Digite um numero de 1 a 7: ");
+            //int dayOfWeek = int.Parse(Console.ReadLine());
+
+            //switch (dayOfWeek) { 
+            //     case 1:
+            //        Console.WriteLine("O dia da semana é: Domingo");
+            //        break;
+            //      case 2:
+            //        Console.WriteLine("O dia da semana é: Segunda-feira");
+            //        break;
+            //      case 3:
+            //        Console.WriteLine("O dia da semana é: Terça-feira");
+            //        break;
+            //      case 4:
+            //        Console.WriteLine("O dia da semana é: Quarta-feira");
+            //        break;
+            //      case 5:
+            //        Console.WriteLine("O dia da semana é: Quinta-feira");
+            //        break;
+            //      case 6:
+            //        Console.WriteLine("O dia da semana é: Sexta-feira");
+            //        break;
+            //      case 7:
+            //        Console.WriteLine("O dia da semana é: Sabado");
+            //        break;
+            //      default:
+            //        Console.WriteLine("Dia inválido. Por favor, digite um número de 1 a 7.");
+            //        break;
+
+            //}
+
+            //ex4
+            Console.WriteLine("=============Desempenho escolar==============");
+            Console.WriteLine("Digite a nota: ");
+            int score = int.Parse(Console.ReadLine());
+
+            switch (score) { 
+                case int n when (n >= 90 && n <= 100):
+                    Console.WriteLine("Desempenho: Excelente");
+                    break;
+                case int n when (n >= 80 && n < 90):
+                    Console.WriteLine("Desempenho: Bom");
+                    break;
+                case int n when (n >= 70 && n < 80):
+                    Console.WriteLine("Desempenho: Regular");
+                    break;
+                default:
+                    Console.WriteLine("Desempenho: Insuficiente");
+                    break;
+            }
+
+
+        }
+    }
+}
