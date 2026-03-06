83761db75b2f4926fc59d844764df0652c683b5b
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-03-06 15:09:12 -0300

    Desenvolvenbdo o while na logica de programação

---
+++ Aula12/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Aula11
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //int i = 0;
+
+            //while (i < 100)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+            double budget = 100;
+
+            while (budget > 0)
+            {
+                Console.WriteLine(budget);
+                budget--;
+            }
+
+            Console.WriteLine("Atenção! Saldo Insuficiente");
+        }
+    }
+}
