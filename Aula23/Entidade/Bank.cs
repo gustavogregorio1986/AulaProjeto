d854a39aca0a1436c03915cdce8858f2690a0b9e
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-03-11 11:17:22 -0300

    Desenvolvimento de metodo com retorno e sem retorno

---
+++ Aula23/Entidade/Bank.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula23.Entidade
+{
+    public class Bank
+    {
+        //1. Atributo
+        public int Ifd { get; set; }
+        public string Name { get; set; }
+
+        //2. Métodos (sem retorno)
+        public void RequestNewCard()
+        {
+            //logica
+            Console.WriteLine("Enviando Enmail..... (Solucite um novo cartão, ligando paar o numero: 8888-8888)");
+        }
+
+    }
+}
---
+++ Aula23/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Aula23.Entidade;
+
+namespace Aula23
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //1. Criara a instância bank
+            var bankPraiano = new Bank();
+
+            bankPraiano.Name = "Banco Praiano";
+            bankPraiano.id = 82545;
+
+            //2. Chamando Método Metodos de instaância da classe Bank
+            bankPraiano.RequestNewCard();
+        }
+    }
+}
