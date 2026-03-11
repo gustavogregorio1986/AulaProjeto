5f441056e3a90b711da5896daef60fd52e87f679
Gustavo Gregorio <gustavogregorio1986@gmail.com>
2026-03-11 13:55:10 -0300

    Desenvolvimento de metodos no projeto

---
+++ Aula24/Entidade/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula24.Entidade
+{
+    public class Person
+    {
+        //1. Atributos
+        public string Name;
+        public int Age;
+
+        //2. Métodos
+        public bool IfLegalPersoon(int age)
+        {
+            if(age >= 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string IfLegalPersoonRefactor(int age)
+        {
+            string resiult = string.Empty;
+
+            if (age >= 18)
+            {
+                resiult = "A pessoa é maior de Idade!";
+                return resiult;
+            }
+            else
+            {
+                resiult = "A pessoa é menor de Idade!";
+                return resiult;
+            }
+        }
+
+        public string IfLegalPersoonRefactorName(int age, string name)
+        {
+            string resiult = string.Empty;
+
+            if (age >= 18)
+            {
+                resiult = "O(a) " +  name + "é maior de Idade!";
+                return resiult;
+            }
+            else
+            {
+                resiult = "O(a) " + name + "é menor de Idade!";
+                return resiult;
+            }
+        }
+    }
+}
---
+++ Aula24/Program.cs
@@ -0,0 +1,40 @@
+﻿using Aula24.Entidade;
+
+namespace Aula24
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //1. Criar instãncia da classe Person
+            Person person = new Person();
+            person.Name = "";
+
+            //2. Métodos
+            Console.WriteLine("Digite uma Idade: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Primeira forma correta");
+
+            var IfLegalPersoon = person.IfLegalPersoon(age);
+            
+            if(IfLegalPersoon == true)
+            {
+                Console.WriteLine("A pessoa é maior de idade");
+            }
+            else
+            {
+                Console.WriteLine("A pessoa é menor de idade");
+            }
+
+            Console.WriteLine("Segunda forma correta");
+
+            //3  Chamando o método e guardando o resultado
+            string response = person.IfLegalPersoonRefactor(age);
+            Console.WriteLine(response);
+
+            string response1 = person.IfLegalPersoonRefactorName(age, person.Name);
+            Console.WriteLine(response1);
+        }
+    }
+}
